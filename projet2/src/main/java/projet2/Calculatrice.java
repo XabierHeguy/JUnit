f6b75f8088ff3c27b2bd834aa93a924e44b76869
XabierHeguy <115142494+XabierHeguy@users.noreply.github.com>
2022-11-03 14:16:17 +0100

    1er commit

---
+++ projet2/src/main/java/projet2/Calculatrice.java
@@ -0,0 +1,14 @@
+package projet2;
+
+public class Calculatrice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+}
