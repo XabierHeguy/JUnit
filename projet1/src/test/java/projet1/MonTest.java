f3d9ff6c9bf6efbd2263c005bbbf3041751a38c4
XabierHeguy <xabier.heguy@wanadoo.fr>
2022-11-03 14:10:08 +0100

    1er commit

---
+++ projet1/src/test/java/projet1/MonTest.java
@@ -0,0 +1,14 @@
+package projet1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MonTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ projet2/src/test/java/projet2/CalculatriceTest.java
@@ -0,0 +1,46 @@
+package projet2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatriceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("BeforeAll 0");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("AfterAll 4");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("BeforeEach 1");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("AfterEach 3");
+	}
+
+	
+	@Test
+	void test1() {
+		System.out.println("Test1 2");
+		
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("Test2 2");
+		
+	}
+
+}
